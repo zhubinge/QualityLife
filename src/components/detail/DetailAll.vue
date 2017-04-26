be92db601ccb24adca09c0872cb93d4a82d567e7
X0IKDNIIGDSEPAM\Administrator <lixue_0809@126.com>
2017-04-26 21:14:39 +0800

    李雪

--- src/components/detail/DetailAll.vue
+++ src/components/detail/DetailAll.vue
@@ -7,12 +7,14 @@
         <input type="text" placeholder="搜索商品">
     </div>
     <div class="h_box" v-for="detailList in detailLists">
-        <p class="h_title">{{detailList.class_name}}</p>
-        <ul class="product">
-            <li class="l" v-for="list in detailList.class_info">
+        <div class="h_title">
+            <p>{{detailList.class_name}}</p>
+        </div>
+        <ul class="product" v-for="list in detailList.class_info">
+            <router-link tag="li" :to="'/detail/class/' + list.info_id" class="l" >
                 <img :src="'static/images/dateilAll/' + list.imgSrc"/>
                 <p class="product_title">{{list.text}}</p>
-            </li>
+            </router-link>
         </ul>
     </div>
     <bottom-nav></bottom-nav>
@@ -28,24 +30,21 @@ export default {
             detailLists:{}
         }
     },
-    components:{
-        BottomNav
-    },
     beforeCreate () {
+        var that = this;//=======================
         this.$http.get('/api/class/getClass')
           .then((response) => {
-<<<<<<< HEAD
             that.detailLists = response.data;
             console.log(that.detailLists)
-=======
->>>>>>> b24098966189a584761dceaed44ace7377568b99
-            this.detailLists = response.data;
-            console.log(this.detailLists)
+
           })
           .catch((reject) => {
             console.log(reject)
           })
-    }
+    },
+    components:{
+        BottomNav
+    },
  }
 </script>
 <style scoped>
@@ -109,11 +108,10 @@ export default {
 }
 .h_box .h_title{
     width: 100%;
-    height: 1.2rem;
-    line-height: 1.2rem;
-    font-size: 18px;
+    height: 1.3rem;
     background: #f2f2f2;
-    text-align: center;
+    line-height: 1.8rem;
+    font-size: 18px;
 }
 .h_box .h_title p{
     width: 100%;
@@ -140,7 +138,7 @@ export default {
 .h_box .product li p{
     height: 0.2rem;
     line-height: 0.2rem;
-    padding-bottom: 0.1rem;
+    padding-bottom: 0.2rem;
     padding-top: .5rem;
     margin:auto;
     text-align: center;
