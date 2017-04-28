d4fcf16e870face9112a0c8665ccad8384be75a6
X0IKDNIIGDSEPAM\Administrator <lixue_0809@126.com>
2017-04-28 11:46:46 +0800

    李雪

--- src/components/Mine.vue
+++ src/components/Mine.vue
@@ -20,6 +20,18 @@
     	<li class="l"><a href="#"><i class="iconfont">&#xe630;</i>回复</a></li>
     	<li class="l borderNone"><a href="#"><i class="iconfont">&#xe629;</i>退换货</a></li>
     </ul>
+    <ul class="myMessage">
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe652;</i>我的订单</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe605;</i>我的优惠券</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe61c;</i>现金积分</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe684;</i>关于发票</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xf0003;</i>密码和登陆信息</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe652;</i>老会员建议</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe80a;</i>客户服务</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe5b4;</i>地址管理</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    	<li><a href="#"><span class="l"><i class="iconfont">&#xe6e2;</i>手机验证</span><span class="r"><i class="iconfont">&#xe609;</i></span></a></li>
+    </ul>
+    <div class="box"></div>
     <bottom-nav></bottom-nav>
 </div>
 </template>
@@ -33,6 +45,10 @@ export default {
 }
 </script>
 <style scoped>
+.box{
+	height: 1rem;
+	width: 100%;
+}
 .mine_nav{
 	width: 100%;
 	height: 1rem;
@@ -40,10 +56,6 @@ export default {
     background: #ffda75;
     color: #fff; 
     position: fixed;
-}
-.box{
-	width: 100%;
-	height: 1rem;
 }
 .mine_nav .mine_nav2{
 	width: 80%;
@@ -66,7 +78,7 @@ export default {
 	text-align: center;
 	font-size: 16px;
 	font-weight: bold;
-	color: #666;
+	color: #fff;
 }
 .loginRegisterChunk .loginRegisterBoxWords{
 	width: 4rem;
@@ -98,15 +110,77 @@ export default {
 }
 .server{
 	width: 100%;
-	height: 2.2rem;
+	overflow: hidden;
+	border-bottom: 1px solid #ccc;
 }
 .server li{
-	width: 27%;
-	height: 0.9rem;
-	border-right: 1px solid #ccc;
-	padding: 0.2rem;
+	width: 33%;
+	height: 0.6rem;
+	padding:0.2rem 0;
 }
-.server li.borderNone{
+.server li a{
+	display: inline-block;
+	width: 100%;
+	height: 100%;
+	border-right: 1px solid #ccc;
+}
+.iconfont{
+	font-size: 20px;
+	margin-right: 0.2rem;
+}
+.server li.borderNone a{
 	border-right: none;
 }
+.myMessage{
+	padding: 0 0.2rem;
+	overflow: auto;
+	width: 100%;
+	box-sizing: border-box;
+}
+.myMessage li{
+	width: 100%;
+	height: 1rem;
+	line-height: 1rem;
+}
+.myMessage li a{
+	display: inline-block;
+	width: 100%;
+	height: 1rem;
+	line-height: 1rem;
+	border-bottom: 1px solid #ccc;
+}
+
+.myMessage li:nth-child(1) span:nth-child(1) .iconfont{
+	color: #00d800;
+}
+.myMessage li:nth-child(2) span:nth-child(1) .iconfont{
+	color: #e94;
+	opacity: 0.5;
+}
+.myMessage li:nth-child(3) span:nth-child(1) .iconfont{
+	color: #f99dee;
+	opacity: 0.5;
+}
+.myMessage li:nth-child(4) span:nth-child(1) .iconfont{
+	color: #99dd00;
+	opacity:0.5;
+}
+.myMessage li:nth-child(5) span:nth-child(1) .iconfont{
+	color: #9699fc;
+}
+.myMessage li:nth-child(6) span:nth-child(1) .iconfont{
+	color: #fe9822;
+}
+.myMessage li:nth-child(7) span:nth-child(1) .iconfont{
+	color: #3df9a3;
+}
+.myMessage li:nth-child(8) span:nth-child(1) .iconfont{
+	color: #f9b462;
+}
+.myMessage li:nth-child(9) span:nth-child(1) .iconfont{
+	color: #ccc;
+    height: 1rem;
+    line-height: 1rem;
+}
+
 </style>
