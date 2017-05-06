780c594c71c2dbb2240abb99faeeec5fd084f23f
DESKTOP-B26F83B\1037624030 <1037624030@qq.com>
2017-05-07 01:34:04 +0800

    朱彬阁

--- server/index.js
+++ server/index.js
@@ -13,9 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(api);
-// 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
+// 访问静态资源文件
 app.use(express.static(path.resolve(__dirname, '../')));
-// 因为是单页应用 所有请求都走/dist/index.html
 // app.get('*', function(req, res) {
 //     const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8');
 //     res.send(html);
